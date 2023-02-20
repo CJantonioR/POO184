78c9f104f185c6528766758ff1108fd19ccaa227
CJantonioR <125703199+CJantonioR@users.noreply.github.com>
2023-02-20 16:59:52 -0600

    Se agreraron los metodos del personaje

--- Personaje.py
+++ Personaje.py
@@ -5,3 +5,19 @@ class Personaje:
     nombre="Master Chief"
     altura="2.70"
     
+    #Metodos Perdonaje
+    
+    def correr(self,status):
+        if(status):
+            print("El personaje"+self.nombre+" esta corriendo")
+        else:
+            print("El personaje"+self.nombre+" se detuvo")
+            
+            def lanzarGranadas(self):
+             print("El personaje"+self.nombre+" Lanzo una granada")   
+                
+        def recargarArma(municiones):
+            cargador = 10
+            cargador=cargador+municiones
+                 print("El arma tiene "+ cargador +" balas") 
+                                 
