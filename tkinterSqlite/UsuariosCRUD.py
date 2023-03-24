a8cada018d4e1f523cf512afcd09e19b026c4b8b
CJantonioR <125703199+CJantonioR@users.noreply.github.com>
2023-03-24 17:13:53 -0600

    agregamos los controladores respetivos y encriptamos las contraseñas en nuestra BD

--- tkinterSqlite/UsuariosCRUD.py
+++ tkinterSqlite/UsuariosCRUD.py
@@ -1,44 +1,47 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from controladorBD import *
 
-ventana= Tk()
+# Creamos una instancia de tipo controlador
+controlador = controladorBD()
+
+# Procedemos a Guardar usando el metodo del objeto controlador
+def ejecutaInsert():
+    controlador.guardarUsuario(varNom.get(), varCor.get(), varCon.get())
+
+ventana = Tk()
 ventana.title("CRUD de Usuarios")
 ventana.geometry("500x300")
 
-panel=ttk.Notebook(ventana)
+panel = ttk.Notebook(ventana)
 panel.pack(fill="both", expand="yes")
 
-pestana1=ttk.Frame(panel)
-pestana2=ttk.Frame(panel)
-pestana3=ttk.Frame(panel)
-pestana4=ttk.Frame(panel)
+pestana1 = ttk.Frame(panel)
+pestana2 = ttk.Frame(panel)
+pestana3 = ttk.Frame(panel)
+pestana4 = ttk.Frame(panel)
 
-#Pestaña1: Formuario de Usuario2
-titulo= Label( pestana1, text="Registro Usuarios", fg="blue", font=("Moder",18)).pack()
-              
-varNom=tk.StringVar()
-lblNom=Label(pestana1, text="Nombre: ").pack()
-txtNom=Entry(pestana1,textvariable=varNom).pack()
+# Pestaña1: Formulario de Usuario2
+titulo = Label(pestana1, text="Registro Usuarios", fg="blue", font=("Moder",18)).pack()
 
-varCor=tk.StringVar()
-lblCor=Label(pestana1, text="Correo: ").pack()
-txtCor=Entry(pestana1,textvariable=varCor).pack()
+varNom = tk.StringVar()
+lblNom = Label(pestana1, text="Nombre: ").pack()
+txtNom = Entry(pestana1,textvariable=varNom).pack()
 
-varCon=tk.StringVar()
-lblCon=Label(pestana1, text="Contraseña: ").pack()
-txtCon=Entry(pestana1,textvariable=varCor).pack()
+varCor = tk.StringVar()
+lblCor = Label(pestana1, text="Correo: ").pack()
+txtCor = Entry(pestana1,textvariable=varCor).pack()
 
-btnGuardas=Button(pestana1, text="Guardad usuario").pack()
-          
+varCon = tk.StringVar()
+lblCon = Label(pestana1, text="Contraseña: ").pack()
+txtCon = Entry(pestana1,textvariable=varCon).pack()
 
+btnGuardas = Button(pestana1, text="Guardad usuario", command=ejecutaInsert).pack()
 
 panel.add(pestana1, text="Formulario de usuario")
 panel.add(pestana2, text="Buscar Usuario")
-panel.add(pestana3,text="Consultar Usuarios")
-panel.add(pestana4,text="Actualizar Usuario")
-
-
+panel.add(pestana3, text="Consultar Usuarios")
+panel.add(pestana4, text="Actualizar Usuario")
 
 ventana.mainloop()
-
--- tkinterSqlite/controladorBD.py
+++ tkinterSqlite/controladorBD.py
@@ -2,3 +2,52 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 import bcrypt 
+
+class controladorBD:
+    
+    def __init__(self):
+        pass
+        
+    #1. preparamos la conexion para usarla cuando sea necesario
+    def conexionBD(self):
+        try:
+            conexion = sqlite3.connect("C:/Users/reach/OneDrive/Documentos/POO184/tkinterSqlite/DBUsuarios.db")
+            print("conectado BD")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+      
+    #Metodo para Insertar      
+    def guardarUsuario(self, nom, cor, con):
+        #1. llamar a la conexion
+        conx = self.conexionBD()
+        
+        #2. Revisar parametros vacios
+        if(nom == "" or cor == "" or con == ""):
+            messagebox.showwarning("Aguas!!", "Revisa este show")
+            conx.close()
+        else:
+            #3. Preparamos los datos y el querySQL
+            cursor = conx.cursor()
+            conH= self.encriptarCon(con)
+            datos = (nom, cor, conH)
+            qrInsert = "insert into TBRegistrados(Nombre, Correo, Contraseñas) values(?,?,?)"
+            
+            #4. Ejecutamos la consulta y cerramos la conexion
+            cursor.execute(qrInsert, datos)
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("Registro", "Registro exitoso")
+            
+    #Metodo para encryptar
+    def encriptarCon(self, con):
+        conPlana= con
+        conPlana= conPlana.encode() #convertimos con a bytes
+        sal= bcrypt.gensalt()
+        
+        conHa= bcrypt.hashpw(conPlana,sal)
+        print(conHa)
+        return conHa
+    
+    
+        
